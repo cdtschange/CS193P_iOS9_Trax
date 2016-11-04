4829137af20ac75612878a0498d1f59595897f20
wei.mao <wei.mao@jimubox.com>
2016-11-04 14:05:48 +0800

    Lession 17 Segues, Core Location, and MapKit

---
+++ Trax/Trax/GPXViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GPXViewController.swift
+//  Trax
+//
+//  Created by Mao on 04/11/2016.
+//  Copyright © 2016 cdts. All rights reserved.
+//
+
+import UIKit
+
+class GPXViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
